e8cb1cc11c038bdecc4a045c5e3186a4bdc04a02
Tom Darshan <36899791+generalSlingshot@users.noreply.github.com>
2018-04-25 11:36:12 +0200

    Add files via upload

---
+++ Sudoku/Oberflaeche.h
@@ -0,0 +1,16 @@
+
+// Prototypen
+int StartMenue(void); // Chung
+void RegistrierungMenue(void); // Henning
+void LoginMenue(void); // Omar
+int HauptMenue(void); // Sven
+void Regelwerk(void); // Sven
+void Bestenliste(); // Tom
+int SudokuSpiel(int schwierigkeitsgrad);
+void printRegistrierungMenue1(void);
+void printRegistrierungMenue2(void);
+void printRegistrierungMenue3(void);
+void RegelwerkPrint(void);
+void LoginMenue(void);
+void printSchwierigkeitsAuswahl(void);
+void printStartMenue(void);
